32e24bd8bb67a6e334d203793a7fdabf18a7c832
Olivia Boumokonia <olivia@imaleo.io>
2019-04-19 15:50:30 +0200

    footer: change copyright

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,10 @@
-<!-- Foote section -->
+
 <footer class="footer-section">
     <div class="contaienr text-center">
-        <p><img src="img/footer-icon.png" alt=""><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        <p>
+            <img src="img/footer-icon.png" alt="">
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits réservés
         </p>
     </div>
 </footer>
-<!-- Foote section end -->
+
