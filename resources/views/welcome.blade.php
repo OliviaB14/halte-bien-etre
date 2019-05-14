f5ac21c6f84feb6b248916cd33aba7fef6b6469b
Olivia Boumokonia <olivia@imaleo.io>
2019-05-14 19:29:00 +0200

    get rid of useless section and comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,6 @@
     </div>
 </section>
 
-<!-- Courses section -->
 <section class="courses-section">
     <div class="container">
         <div class="row">
@@ -52,31 +51,7 @@
         </div>
     </div>
 </section>
-<!-- Courses section end -->
 
-
-<!-- Meditation section -->
-<section class="meditation-section set-bg" data-setbg="img/meditation-bg.png">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="meditation-content">
-                    <div class="sp-title text-white">
-                        <span>Begginer & Advanced</span>
-                        <h4>Meditation Sessions</h4>
-                    </div>
-                    <p>Etiam pretium, nibh vitae varius ultricies, tellus diam gravida elit, vel rhoncus eros eros sit amet ipsum. Cras cursus pellentesque tortor. Ut pellentesque auctor lorem, at maximus lacus faucibus nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris auctor nunc non nulla euismod consequat. Pellentesque non mattis nulla. Fusce quis tempor risus, non elementum dui. Curabitur et mattis ex, a ultrices lorem. </p>
-                    <a href="#" class="site-btn">discover</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <img src="img/meditation-img.png" class="meditation-img" alt="">
-</section>
-<!-- Meditation section end -->
-
-
-<!-- Timetable section -->
 <section class="timetable-section spad">
     <div class="container">
         <div class="row">
@@ -132,10 +107,7 @@
         </div>
     </div>
 </section>
-<!-- Timetable section end -->
 
-
-<!-- Testimonial section -->
 <section class="testimonial-section spad set-bg" data-setbg="img/testimonial-bg.jpg">
     <img src="img/testimonial-img-1.png" class="testimonial-img-1" alt="">
     <img src="img/testimonial-img-2.png" class="testimonial-img-2" alt="">
@@ -161,32 +133,8 @@
         </div>
     </div>
 </section>
-<div class="contact-section">
-    <div class="map-warp">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20536.469437823635!2d1.1709809252100172!3d49.954001732221485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e0a6c1061b26a9%3A0x40c14484fb68f70!2s76370+Berneval-le-Grand!5e0!3m2!1sfr!2sfr!4v1557431309096!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-6 offset-lg-6">
-                <div class="contact-box contact-text">
-                    <div class="sp-title">
-                        <span>Begginer & Advanced</span>
-                        <h4>Meditation Sessions</h4>
-                    </div>
-                    <h6>Information</h6>
-                    <ul>
-                        <li><a href="http://www.facebook.com/lahaltebienetre">La Halte au Bien-Être sur Facebook</a></li>
-                        <li><a href="tel:0664054024">06.64.05.40.24</a></li>
-                        <li><a href="mailto:lahaltebienetre@gmail.com">lahaltebienetre@gmail.com</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Contact section end -->
 
 
-<!-- Gallery section -->
 <div class="gallery-section">
     <div class="gallery-slider owl-carousel">
         <div class="gallery-item">
@@ -206,7 +154,5 @@
         </div>
     </div>
 </div>
-<!-- Gallery section end -->
 
 @endsection
-
